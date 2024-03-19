90fff5a104794ccbd5d4d94b99c05537034d6969
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-03-19 11:36:10 +0700

    Membuat Prosedur dan fungsi Persegi Panjang

---
+++ 155_Prosedur dan fungsi Persegi Panjang/155_Prosedur dan fungsi Persegi Panjang.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void inputdata() {
+    cout << "Masukan nilai panjang : ";
+    cin >> panjang;
+    cout << "Masukan nilai lebar : ";
+    cin >> lebar;
+}
+int hitungluas() {
+    return panjang * lebar;
+}
+
+void bebas() {
+    cout << " luasnya adalah : " << hitungluas() << endl;
+}
+int main() {
+    inputdata();
+    bebas();
+
+
+
+}
+
+
